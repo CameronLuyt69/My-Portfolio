c5be9d5df34994d2787a8cc82ca672a76c735709
CAMERON LUYT <cameronluyt30@gmail.com>
2020-08-05 09:53:38 +0200

    Updates

--- precache-manifest.3e09dee1d001a67c6cd242f3ef822b95.js
+++ precache-manifest.3e09dee1d001a67c6cd242f3ef822b95.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a5789c94c97972779475b3672d8b508",
+    "revision": "fd69734b28b01c51621aea36baecbc3b",
     "url": "/My-Portfolio/index.html"
   },
   {
-    "revision": "073e31fbfa2caa64a6f4",
+    "revision": "e9e872a9765dde7d7371",
     "url": "/My-Portfolio/static/css/main.d498bfb3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/My-Portfolio/static/js/2.9d16067d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "073e31fbfa2caa64a6f4",
-    "url": "/My-Portfolio/static/js/main.415d01c2.chunk.js"
+    "revision": "e9e872a9765dde7d7371",
+    "url": "/My-Portfolio/static/js/main.b7edf225.chunk.js"
   },
   {
     "revision": "bd26aa8a63ab866d9120",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/My-Portfolio/precache-manifest.bef1b2476a30139c7d28a42134780471.js"
+  "/My-Portfolio/precache-manifest.3e09dee1d001a67c6cd242f3ef822b95.js"
 );
 
 self.addEventListener('message', (event) => {
